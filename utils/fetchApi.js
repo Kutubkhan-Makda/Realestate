02b0fdda5d6d18109a08130accab56474e2ec207
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-31 23:37:46 +0530

    day-2

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const BaseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const {data} = await axios.get((url),{
+        headers: {
+            'X-RapidAPI-Key': '7e56f3663bmsh9cab74bf04c9828p1b0d38jsn0fe9bdc90444',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+          }
+    })
+}
