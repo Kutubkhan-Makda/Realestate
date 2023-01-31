c7c76fd843639418e703228c966e845fa09a697d
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-31 23:21:09 +0530

    day-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </Head>
       <Box>
         <Banner purpose='RENT A HOME' title1='Rental Homes for' title2='Everyone' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Renting' linkName='/search?purpose=for-rent' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'/>
-        <Flex></Flex>
+        <Flex flexWrap='wrap'></Flex>
         <Banner purpose='BUY A HOME' title1='Find, Buy & Own Your' title2='Dream Home' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Buying' linkName='/search?purpose=for-sale' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'/>
       </Box>
     </>
