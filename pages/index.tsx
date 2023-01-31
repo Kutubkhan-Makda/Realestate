e6930bbc02a8654895cb3a1f18b815532997e176
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-31 22:20:42 +0530

    day-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
 import { Flex } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/button'
+import Link from 'next/link'
 
-const Banner = ({purpose})=>(
+const Banner = ({purpose,title1,title2,desc1,desc2,imageUrl,linkName,buttonText})=>(
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    {purpose}
+    <Image src={imageUrl} width={500} height={300} alt='banner'/>
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{title1}<br/>{title2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br/>{desc2}</Text>
+      <Button fontSize='xl' bg='blue.300' color='white'>
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
   </Flex>
 )
 
