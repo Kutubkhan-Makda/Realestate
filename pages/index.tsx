4c117c585e5c2d877d776c61fd9f35bf63e87bbe
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-31 23:15:06 +0530

    day-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Button } from '@chakra-ui/react'
 import { Box, Text } from '@chakra-ui/layout'
-import { Button } from '@chakra-ui/button'
 import Link from 'next/link'
 
 const Banner = ({purpose,title1,title2,desc1,desc2,imageUrl,linkName,buttonText})=>(
@@ -12,7 +11,7 @@ const Banner = ({purpose,title1,title2,desc1,desc2,imageUrl,linkName,buttonText}
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br/>{title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br/>{desc2}</Text>
-      <Button fontSize='xl' bg='blue.300' color='white'>
+      <Button fontSize='xl'>
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
@@ -28,10 +27,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <Box>
         <Banner purpose='RENT A HOME' title1='Rental Homes for' title2='Everyone' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Renting' linkName='/search?purpose=for-rent' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'/>
-        <Banner purpose='BUY A HOME' title1='Find, Buy & Own Your' title2='Dream Home' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Renting' linkName='/search?purpose=for-rent' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'/>
-      </main>
+        <Flex></Flex>
+        <Banner purpose='BUY A HOME' title1='Find, Buy & Own Your' title2='Dream Home' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Buying' linkName='/search?purpose=for-sale' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'/>
+      </Box>
     </>
   )
 }
