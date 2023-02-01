a9e45c4cbbd10ab24265a33e1a9b509bb8c4a43e
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-01 21:20:24 +0530

    day-3

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,9 @@ export default function Home({propertyForSale,propertyForRent}) {
           {propertyForRent.map((property)=><Property property={property} key={property.id}/>)}
         </Flex>
         <Banner purpose='BUY A HOME' title1='Find, Buy & Own Your' title2='Dream Home' desc1='Explore Apartments, Villas, Homes' desc2='and more' buttonText='Explore Buying' linkName='/search?purpose=for-sale' imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'/>
-        {propertyForSale.map((property)=><Property property={property} key={property.id}/>)}
+        <Flex flexWrap='wrap'>
+          {propertyForSale.map((property)=><Property property={property} key={property.id}/>)}
+        </Flex>
       </Box>
     </>
   )
