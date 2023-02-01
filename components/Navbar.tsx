1b6c77118b73d04ff9702dc8397d372c699fc66f
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-01 21:29:23 +0530

    day-3

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,7 @@
+import {FcMenu,FcHome,FcAbout} from 'react-icons/fc'
+import {BsSearch} from 'react-icons/bs'
+import {FiKey} from 'react-icons/fi'
+
 function Navbar() {
   return (
     <div>Navbar</div>
