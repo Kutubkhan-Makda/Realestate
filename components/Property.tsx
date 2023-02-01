26e76c30e0625ec4326bed249ee2baa872140054
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-01 20:07:51 +0530

    day-3

--- components/Property.tsx
+++ components/Property.tsx
@@ -1,7 +1,11 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 import Image from "next/image"
 import Link from "next/link"
+import {GoVerified} from 'react-icons/go'
+import {FaBath,FaBed} from 'react-icons/fa'
+import {BsGridFill} from 'react-icons/bs'
 import defaultImage from '@/assets/house.jpg'
+import millify from "millify"
 
 export const Property = ({property:{coverPhoto,price,rentFrequency,rooms,title,baths,area,agency,isVerified,externalId}}) => {
   return (
@@ -10,8 +14,22 @@ export const Property = ({property:{coverPhoto,price,rentFrequency,rooms,title,b
         <Box>
           <Image src={coverPhoto?coverPhoto.url:defaultImage} width={400} height={260} alt='house'/>
         </Box>
-        <Box>
-          
+        <Box w='full'>
+          <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+            <Flex alignItems='center'>
+              <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified/>}</Box>
+              <Text fontWeight='bold' fontSize='lg'>AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+            </Flex>
+            <Box>
+              <Avatar size='sm' src={agency?.logo?.url}/>
+            </Box>
+          </Flex>
+          <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+            {rooms} <FaBed/> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/>
+          </Flex>
+          <Text fontSize='lg'>
+            {title}
+          </Text>
         </Box>
       </Flex>
     </Link>
