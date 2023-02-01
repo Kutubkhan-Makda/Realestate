2f61e18e6d444d96271a8ff6dafcfc6e931c239a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-01 21:42:53 +0530

    day-3

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,27 @@
 import {FcMenu,FcHome,FcAbout} from 'react-icons/fc'
 import {BsSearch} from 'react-icons/bs'
 import {FiKey} from 'react-icons/fi'
+import { Box, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer } from '@chakra-ui/react'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Flex p='2' borderBottom='1px' borderColor='gray.100'>
+        <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
+            <Link href='/' paddingLeft='2'>Realtor</Link>
+        </Box>
+        <Spacer/>
+        <Box>
+            <Menu>
+                <MenuButton as={IconButton} icon={<FcMenu/>} variant='outlined' color='red.400'/>
+                <MenuList>
+                    <Link href='/' passHref>
+                        <MenuItem icon={<FcHome/>}>Home</MenuItem>
+                    </Link>
+                </MenuList>
+            </Menu>
+        </Box>
+    </Flex>
   )
 }
 
