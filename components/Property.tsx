7c46b997725a3cdf1cab635b9f78029da85d7cac
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-01 19:14:27 +0530

    day-3

--- components/Property.tsx
+++ components/Property.tsx
@@ -1,9 +1,14 @@
+import { Box, Flex } from "@chakra-ui/react"
 import Link from "next/link"
 
-export const Property = ({property:{coverPhoto,price,rentFrequency,rooms,}}) => {
+export const Property = ({property:{coverPhoto,price,rentFrequency,rooms,title,baths,area,agency,isVerified,externalId}}) => {
   return (
-    <Link href={`/property/${externalID}`} passHref>
-
+    <Link href={`/property/${externalId}`} passHref>
+      <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0' justifyContent='flex-start' cursor='pointer'>
+        <Box>
+          
+        </Box>
+      </Flex>
     </Link>
   )
 }
