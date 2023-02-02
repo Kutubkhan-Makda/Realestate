21ff57553002133801d2260ccc3d42401746b7cc
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-02 19:33:36 +0530

    day-4

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <Flex p='2' borderBottom='1px' borderColor='gray.100'>
         <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-            <Link href='/' paddingLeft='2'>Realtor</Link>
+            <Link href='/'>Realtor</Link>
         </Box>
         <Spacer/>
         <Box>
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,13 +1,18 @@
 import {BsFilter} from 'react-icons/bs';
 import {useState} from 'react';
 import { useRouter } from 'next/router';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 function search() {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
   return (
-    <div>search</div>
+    <Box>
+      <Flex cursor='pointer' bg='gray.100' borderBottom='1px' borderColor='gray.200' p='2' fontWeight='black'>
+        <Text>Search Property by Filter</Text>
+      </Flex>
+    </Box>
   )
 }
 
