cc1deec6d1ea5261f0f267702279eb287eabbd9d
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-02 17:04:33 +0530

    day-4

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,4 +1,11 @@
+import {BsFilter} from 'react-icons/bs';
+import {useState} from 'react';
+import { useRouter } from 'next/router';
+
 function search() {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
+
   return (
     <div>search</div>
   )
