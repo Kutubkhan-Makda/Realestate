66d37f0329d34730f9fdca44ff23eecc7a86a02b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-02 23:54:18 +0530

    day-4

--- components/SearchFilters.tsx
+++ components/SearchFilters.tsx
@@ -1,13 +1,18 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import {useState} from 'react';
 import {MdCancel} from 'react-icons/md'
+import {filterData} from '@/utils/filterData'
 
 function SearchFilters() {
-    const [filters, setFilters] = useState({});
+    const [filters, setFilters] = useState(filterData);
 
   return (
     <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
-        
+        {filters.map((filter)=>(
+            <Box key={filter.queryName}>
+
+            </Box>
+        ))}
     </Flex>
   )
 }
