32c5838a60a04a6a81a94e4ac0f14570b213de37
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 21:59:47 +0530

    day-5

--- components/ImageScrollbar.tsx
+++ components/ImageScrollbar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import {useContext} from 'react'
 import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu'
 import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 const LeftArrow = ()=>{
   const {scrollPrev} = useContext(VisibilityContext)
@@ -22,9 +23,24 @@ const RightArrow = ()=>{
   )
 }
 
+function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+  const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+  if (isThouchpad) {
+    ev.stopPropagation();
+    return;
+  }
+
+  if (ev.deltaY < 0) {
+    apiObj.scrollNext();
+  } else if (ev.deltaY > 0) {
+    apiObj.scrollPrev();
+  }
+}
+
 function ImageScrollbar({data}) {
   return (
-    <ScrollMenu style={{overflow:'hidden'}} LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    <ScrollMenu style={{overflow:'hidden'}} onWheel={onWheel} LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item)=>(
         <Box width='910px' key={item.id} itemID={item.id} overflow='hidden' p='1'>
           <Image alt='property' placeholder='blur' blurDataURL={item.url} src={item.url} width={1000} height={500} sizes='(max-width:500px) 100px,(max-width):1023px 400px, 1000px'/>
--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,14 +1,43 @@
 import {GoVerified} from 'react-icons/go'
 import {FaBath,FaBed} from 'react-icons/fa'
 import {BsGridFill} from 'react-icons/bs'
-import { Box } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 import {fetchApi,BaseUrl} from '@/utils/fetchApi'
 import ImageScrollbar from '@/components/ImageScrollbar'
+import millify from 'millify'
 
 function PropertyDetails({PropertyDetails:{price,rentFrequency,rooms,title,baths,area,agency,isVerified,description,type,purpose,furnishingStatus,amenities,photos}}) {
   return (
     <Box maxWidth='1000px' margin='auto' p='4'>
         {photos && <ImageScrollbar data={photos}/>}
+        <Box w='full' p='6'>
+          <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+            <Flex alignItems='center'>
+              <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified/>}</Box>
+              <Text fontWeight='bold' fontSize='lg'>AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+            </Flex>
+            <Box>
+              <Avatar size='sm' src={agency?.logo?.url}/>
+            </Box>
+          </Flex>
+          <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+            {rooms} <FaBed/> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/>
+          </Flex>
+          <Box marginTop='2'>
+            <Text fontSize='lg' marginBottom='2' fontWeight='bold'>
+              {title}
+            </Text>
+            <Text lineHeight='2' color='gray.600'>
+              {description}
+            </Text>
+          </Box>
+          <Flex flexWrap='wrap' textTransform='uppercase' justifyContent='space-between'>
+            <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3'>
+              <Text>Type</Text>
+              <Text fontWeight='bold'>{type}</Text>
+            </Flex>
+          </Flex>
+        </Box>
     </Box>
   )
 }
