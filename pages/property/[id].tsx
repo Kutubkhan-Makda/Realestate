06197eb8a6a270376a8d9ec3bb311593a69b2cc5
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 19:26:53 +0530

    day-5

---
+++ pages/property/[id].tsx
@@ -0,0 +1,24 @@
+import {GoVerified} from 'react-icons/go'
+import {FaBath,FaBed} from 'react-icons/fa'
+import {BsGridFill} from 'react-icons/bs'
+import { Box } from '@chakra-ui/react'
+import {fetchApi,BaseUrl} from '@/utils/fetchApi'
+
+function PropertyDetails({PropertyDetails:{price,rentFrequency,rooms,title,baths,area,agency,isVerified,description,type,purpose,furnishingStatus,amenities,photos}}) {
+  return (
+    <Box maxWidth='1000px' margin='auto' p='4'>
+        {PropertyDetails}
+    </Box>
+  )
+}
+
+export default PropertyDetails
+
+export async function getServerSideProps({params:{id}}){
+    const data = await fetchApi(`${BaseUrl}/properties/detail?externalID=${id}`);
+    return{
+        props:{
+            PropertyDetails:data
+        }
+    }
+}
