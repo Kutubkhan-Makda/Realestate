0379933e80918b65bd06fb5d239088cd211290f8
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 19:48:32 +0530

    day-5

--- components/ImageScrollbar.tsx
+++ components/ImageScrollbar.tsx
@@ -1,6 +1,22 @@
+import { Flex } from '@chakra-ui/react'
+import {useContext} from 'react'
+import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu'
+import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
+
+const LeftArrow = ()=>{
+  const {scrollPrev} = useContext(VisibilityContext)
+  return (
+    <Flex justifyContent='center' alignItems='center' marginRight='1'>
+      
+    </Flex>
+  )
+}
+
 function ImageScrollbar() {
   return (
-    <div>ImageScrollbar</div>
+    <ScrollMenu style={{overflow:'hidden'}} LeftArrow={LeftArrow} RightArrow={RightArrow}>
+
+    </ScrollMenu>
   )
 }
 
