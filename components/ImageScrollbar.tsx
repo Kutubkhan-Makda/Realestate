f637fac4768988c249d2093def6b0c4303b4d0e7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 20:23:54 +0530

    day-5

--- components/ImageScrollbar.tsx
+++ components/ImageScrollbar.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex,Icon } from '@chakra-ui/react'
+import Image from 'next/image'
 import {useContext} from 'react'
 import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu'
 import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
@@ -7,7 +8,7 @@ const LeftArrow = ()=>{
   const {scrollPrev} = useContext(VisibilityContext)
   return (
     <Flex justifyContent='center' alignItems='center' marginRight='1'>
-      <Icon as={FaArrowAltCircleLeft} onClick={scrollPrev} fontSize='2xl' cursor='pointer'/>
+      <Icon as={FaArrowAltCircleLeft} onClick={() => scrollPrev()} fontSize='2xl' cursor='pointer'/>
     </Flex>
   )
 }
@@ -16,7 +17,7 @@ const RightArrow = ()=>{
   const {scrollNext} = useContext(VisibilityContext)
   return (
     <Flex justifyContent='center' alignItems='center' marginRight='1'>
-      <Icon as={FaArrowAltCircleRight} onClick={scrollNext} fontSize='2xl' cursor='pointer'/>
+      <Icon as={FaArrowAltCircleRight} onClick={() => scrollNext()} fontSize='2xl' cursor='pointer'/>
     </Flex>
   )
 }
@@ -24,10 +25,10 @@ const RightArrow = ()=>{
 function ImageScrollbar({data}) {
   return (
     <ScrollMenu style={{overflow:'hidden'}} LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {data.map((image)=>(
-        <Box width='910px' itemID={item.id} overflow='hidden'>
-
-        </Box>
+      {data.map((item)=>(
+        <Box width='910px' key={item.id} itemID={item.id} overflow='hidden' p='1'>
+          <Image alt='property' placeholder='blur' blurDataURL={item.url} src={item.url} width={1000} height={500} sizes='(max-width:500px) 100px,(max-width):1023px 400px, 1000px'/>
+        </Box> 
       ))}
     </ScrollMenu>
   )
--- components/Property.tsx
+++ components/Property.tsx
@@ -7,9 +7,9 @@ import {BsGridFill} from 'react-icons/bs'
 import defaultImage from '@/assets/house.jpg'
 import millify from "millify"
 
-function Property({property:{coverPhoto,price,rentFrequency,rooms,title,baths,area,agency,isVerified,externalId}}) {
+function Property({property:{coverPhoto,price,rentFrequency,rooms,title,baths,area,agency,isVerified,externalID}}) {
   return (
-    <Link href={`/property/${externalId}`} passHref>
+    <Link href={`/property/${externalID}`} passHref>
       <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0' justifyContent='flex-start' cursor='pointer'>
         <Box>
           <Image src={coverPhoto?coverPhoto.url:defaultImage} width={400} height={260} alt='house'/>
