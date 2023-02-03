950033c846f003011bec7dfa97aea9866846d0a0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 20:07:06 +0530

    day-5

--- components/ImageScrollbar.tsx
+++ components/ImageScrollbar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex,Icon } from '@chakra-ui/react'
 import {useContext} from 'react'
 import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu'
 import {FaArrowAltCircleLeft,FaArrowAltCircleRight} from 'react-icons/fa'
@@ -7,15 +7,28 @@ const LeftArrow = ()=>{
   const {scrollPrev} = useContext(VisibilityContext)
   return (
     <Flex justifyContent='center' alignItems='center' marginRight='1'>
-      
+      <Icon as={FaArrowAltCircleLeft} onClick={scrollPrev} fontSize='2xl' cursor='pointer'/>
     </Flex>
   )
 }
 
-function ImageScrollbar() {
+const RightArrow = ()=>{
+  const {scrollNext} = useContext(VisibilityContext)
+  return (
+    <Flex justifyContent='center' alignItems='center' marginRight='1'>
+      <Icon as={FaArrowAltCircleRight} onClick={scrollNext} fontSize='2xl' cursor='pointer'/>
+    </Flex>
+  )
+}
+
+function ImageScrollbar({data}) {
   return (
     <ScrollMenu style={{overflow:'hidden'}} LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {data.map((image)=>(
+        <Box width='910px' itemID={item.id} overflow='hidden'>
 
+        </Box>
+      ))}
     </ScrollMenu>
   )
 }
