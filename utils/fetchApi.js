d811e58b8411fa55536d61569d4f05054ebed2b4
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-03 09:08:21 +0530

    day-5

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const BaseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url),{
         headers: {
-            'X-RapidAPI-Key': '7e56f3663bmsh9cab74bf04c9828p1b0d38jsn0fe9bdc90444',
+            'X-RapidAPI-Key': process.env.NEXT_RAPID_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
           }
     });
